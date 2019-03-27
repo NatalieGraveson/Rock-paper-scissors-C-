53a2bb4def5f8c2cd8fbd6a9d4ee402c524f4ab3
Natalie Graveson <nataliemgraveson@gmail.com>
2019-03-27 16:46:08 -0600

    working

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace rockit
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+
+
+
+      Console.WriteLine("Rock Paper Scissors!");
+      bool playing = true;
+      Random rand = new Random();
+      while (playing)
+      {
+        System.Console.Write("enter choice :");
+        string input = Console.ReadLine().ToLower();
+        string[] arrInputs = { "rock", "paper", "scissor" };
+        if (Array.IndexOf(arrInputs, input) == -1)
+        {
+          Console.WriteLine("TRY HARDER");
+          continue;
+        }
+        string[] arrRes = { "rock", "paper", "scissor" };
+        int index = rand.Next(arrRes.Length);
+        Console.WriteLine($"Computer Choice:{arrRes[index]}");
+        if (index == 0 && input == "rock")
+        {
+          Console.WriteLine("TIE");
+        }
+
+      }
+    }
+  }
+}
+
